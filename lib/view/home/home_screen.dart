fb517bdcb8c18c7f2414d66b35b072cbb3efd45b
ketanvishwakarma <ketanvishwakarma@gmail.com>
2023-03-18 23:50:48 +0530

    Refactor: Build list to Article list

--- lib/view/home/home_screen.dart
+++ lib/view/home/home_screen.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:news_app/view/home/widgets/build_articles.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_app/blocs/article_list/article_list_bloc.dart';
+import 'package:news_app/blocs/bookmark_list/bookmark_list_bloc.dart';
+import 'package:news_app/core/di/service_locator.dart';
+import 'package:news_app/data/bookmark_repository.dart';
+import 'package:news_app/data/news_repository.dart';
+import 'package:news_app/view/home/widgets/article_list.dart';
 import 'package:news_app/view/home/widgets/custom_app_bar.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -7,17 +13,27 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: CustomAppBar(),
-        body: TabBarView(
-          children: [
-            BuildArticles(),
-            Center(
-              child: Text('Bookmarked'),
-            ),
-          ],
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) =>
+              ArticleListBloc(newsRepository: getIt.get<NewsRepository>())
+                ..add(const ArticleFetchRequested()),
+        ),
+        BlocProvider(
+          create: (context) =>
+              BookmarkListBloc(getIt.get<BookmarkRepository>()),
+        ),
+      ],
+      child: const DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: CustomAppBar(),
+          body: TabBarView(
+            children: [
+              ArticleList(),
+            ],
+          ),
         ),
       ),
     );
--- lib/view/home/widgets/article_list.dart
+++ lib/view/home/widgets/article_list.dart
@@ -1,36 +1,19 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:news_app/blocs/article_list/article_list_bloc.dart';
-import 'package:news_app/core/di/service_locator.dart';
-import 'package:news_app/data/news_repository.dart';
 import 'package:news_app/view/home/widgets/article_card.dart';
 
-class BuildArticles extends StatelessWidget {
-  const BuildArticles({
+class ArticleList extends StatefulWidget {
+  const ArticleList({
     super.key,
   });
 
   @override
-  Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) =>
-          ArticleListBloc(newsRepository: getIt.get<NewsRepository>())
-            ..add(const ArticleFetchRequested()),
-      child: const _BuildArticlesList(),
-    );
-  }
+  State<ArticleList> createState() => _ArticleListState();
 }
 
-class _BuildArticlesList extends StatefulWidget {
-  const _BuildArticlesList();
-
-  @override
-  State<_BuildArticlesList> createState() => _BuildArticlesListState();
-}
-
-class _BuildArticlesListState extends State<_BuildArticlesList> {
+class _ArticleListState extends State<ArticleList> {
   final _scrollController = ScrollController();
 
   @override
